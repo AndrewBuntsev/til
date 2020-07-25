3b1a100b8a8b6c792816f1477d4ce27d5d0479f9
andreibuntsev <andrew.buntsev@gmail.com>
2020-07-25 12:04:47 +1000

    Update server IP address

--- client/src/const/settings.js
+++ client/src/const/settings.js
@@ -1,5 +1,5 @@
-export const ENDPOINT = 'http://ec2-13-211-212-225.ap-southeast-2.compute.amazonaws.com:12346';
-export const THIS_URL = 'http%3A%2F%2Fec2-13-211-212-225.ap-southeast-2.compute.amazonaws.com:3000';
+export const ENDPOINT = 'http://54.66.53.83:12346';
+export const THIS_URL = 'http%3A%2F%2F54.66.53.83:3000';
 
 //export const ENDPOINT = 'http://192.168.1.101:12346';
 //export const THIS_URL = 'https%3A%2F%2Flocalhost:3000';
