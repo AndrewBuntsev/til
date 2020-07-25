30e8a22afba11644f9ffdaab2efea2e1dff9d7b4
andreibuntsev <andrew.buntsev@gmail.com>
2020-07-25 14:18:31 +1000

    Replaced port number from 3000 to 80

--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/til/client 
+sed -i 's/3000/80/g' node_modules/react-scripts/scripts/start.js
 pm2 start node_modules/react-scripts/scripts/start.js &
 
--- server/const/settings.js
+++ server/const/settings.js
@@ -1,3 +1,3 @@
-exports.WEB_URL = 'http%3A%2F%2F54.66.53.83:3000';
+exports.WEB_URL = 'http%3A%2F%2Ftoday-i-learned.org:80';
 
 //exports.WEB_URL = 'https%3A%2F%2Flocalhost:3000';
