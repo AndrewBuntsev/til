da500d9cdd5f9e43345a95431eb1f87f3852bb6f
andreibuntsev <andrew.buntsev@gmail.com>
2020-07-25 12:51:29 +1000

    updated pm2 scripts

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -2,4 +2,5 @@
 cd /home/ec2-user/til/server && npm install
 cd /home/ec2-user/til/client && npm install
 cp /home/ec2-user/til/.env /home/ec2-user/til/server/.env
-cd /home/ec2-user/til/server && pm2 start server.js &
+cd /home/ec2-user/til/server 
+pm2 start server.js &
--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/til/client && pm2 start node_modules/react-scripts/scripts/start.js &
+cd /home/ec2-user/til/client 
+pm2 start node_modules/react-scripts/scripts/start.js &
 
