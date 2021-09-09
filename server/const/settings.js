9c89233370b1a1801e1ebc249b1c821a52ed0a02
Andrei Buntsev <andrew.buntsev@gmail.com>
2021-09-10 08:40:53 +1000

    Updated cognito credentials for CloudFront deploy

--- server/const/settings.js
+++ server/const/settings.js
@@ -1,6 +1,13 @@
-exports.WEB_URL = 'https%3A%2F%2Ftoday-i-learned.org';
+// local
 //exports.WEB_URL = 'https%3A%2F%2Flocalhost';
-exports.COGNITO_CLIENT_ID = '438vn2gdv98kv5bu1viqvqmgjc';
 //exports.COGNITO_CLIENT_ID = '3u9578d8jvrnk2aa0erhnocd1n';
 
-//exports.WEB_URL = 'https%3A%2F%2Flocalhost:3000';
+// prod
+// exports.WEB_URL = 'https%3A%2F%2Ftoday-i-learned.org';
+// exports.COGNITO_CLIENT_ID = '438vn2gdv98kv5bu1viqvqmgjc';
+
+
+// cloudfront
+exports.WEB_URL = 'https%3A%2F%2Fd2tha6ht526vyi.cloudfront.net';
+exports.COGNITO_CLIENT_ID = '2dr4kjb7btvf0qqfv0vfkdajon';
+
