c9702e8728c905fe271b8dad0f84439b7a358045
Andrei Buntsev <andrew.buntsev@gmail.com>
2021-09-10 18:10:46 +1000

    Deleted deploy_scripts

--- deploy_scripts/app_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/til/client 
-sed -i 's/3000/443/g' node_modules/react-scripts/scripts/start.js
-pm2 start node_modules/react-scripts/scripts/start.js &
-
--- deploy_scripts/before_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -r /home/ec2-user/til/server
-rm -r /home/ec2-user/til/client
